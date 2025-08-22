14268b1b09f68ad83dbbb5e71ec6ea841de88428
Matheus Melo <matheusfernando.vm@gmail.com>
2025-08-22 10:18:55 -0300

    Finalizando o Exercício 2 da lista 1

--- screens/Um.tsx
+++ screens/Um.tsx
@@ -3,26 +3,36 @@ import { View, StyleSheet } from "react-native";
 import Constants from "expo-constants";
 
 export default function Um() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.topo} />
-            <View style={styles.base} />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.topo}>
+        <View style={styles.caixaLime} />
+        <View style={styles.caixaAquamarine} />
+      </View>
+      <View style={styles.base} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: "column",
-        paddingTop: Constants.statusBarHeight,
-    },
-    topo: {
-        flex: 0.5,
-        backgroundColor: "crimson"
-    },
-    base: {
-        flex: 0.5,
-        backgroundColor: "salmon"
-    },
+  container: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+  },
+  topo: {
+    flex: 0.5,
+    flexDirection: "row",
+  },
+  caixaLime: {
+    flex: 0.5,
+    backgroundColor: "lime",
+  },
+  caixaAquamarine: {
+    flex: 0.5,
+    backgroundColor: "aquamarine",
+  },
+  base: {
+    flex: 0.5,
+    backgroundColor: "salmon",
+  },
 });
